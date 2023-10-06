003e5d697a1970464e85aa47df401957a7867d2a
Jon Boone <ipmonger@delamancha.org>
2023-10-06 18:25:05 -0400

    update with initial code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tic-tac-toe
+
+## This is an implementation of the tic-tac-toe pair programming task for applying to the Recurse Center
---
+++ tic-tac-toe.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#
+# tic-tac-toe.py
+#
+# Author: Jon Boone <ipmonger@delamancha.org>
+#
+
+if __name__ == "__main__":
+    print(f"hello tic-tac-toe!\n", end="")
