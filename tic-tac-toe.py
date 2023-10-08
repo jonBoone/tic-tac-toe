681e44256e4bb9f7cfb522f97a7f26cae168e3e9
Jon Boone <ipmonger@delamancha.org>
2023-10-08 16:07:29 -0400

    implementation of the Marker class associatd with a player

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,5 +6,15 @@
 # Author: Jon Boone <ipmonger@delamancha.org>
 #
 
+class Marker:
+    """The marker for a particular player """
+
+    def __init__(self, symbol: str):
+        self.symbol = symbol
+
+    def __str__(self):
+        return str(self.symbol)
+
+
 if __name__ == "__main__":
     print("hello tic-tac-toe!\n", end="")
